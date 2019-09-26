24fc4cd154140dc27cd59c7615becaf1efab66cf
Gustavo <gudamota@gmail.com>
2019-09-25 21:52:49 -0300

    criando Model do Fornecedor

--- ProjetoLivraria/src/model/Fornecedores.java
+++ ProjetoLivraria/src/model/Fornecedores.java
@@ -8,4 +8,17 @@ public class Fornecedores {
 	private int cnpj;
 	private String email;
 	private int telefone;
-	private int celular;}
+	private int celular;
+	
+	
+	public Fornecedores(){
+        System.out.println("Construtor do Fornecedor\n");
+	}
+	public String getNomeFornecedor() {
+	        return nmFornecedor;
+	}
+	 
+	public void setNomeFornecedor(String nmFornecedor) {
+	        this.nmFornecedor = nmFornecedor;
+	}
+} 
