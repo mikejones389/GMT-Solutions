0af33b51ce171598afacacc4b45aa96ec6a52fa9
Gustavo <gudamota@gmail.com>
2019-09-25 21:26:30 -0300

    criando o Model do Fornecedor

---
+++ ProjetoLivraria/src/model/Fornecedores.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Fornecedores {
+	private int cdFornecdor;
+	private String nmFornecedor;
+	private String nmFantasia;
+	private String rzSocial;
+	private int cnpj;
+	private String email;
+	private int telefone;
+	private int celular;}
