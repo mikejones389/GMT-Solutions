abbb573c5552fa3237bef2cd27339be7567bff11
Mike <mike.silva.jones@gmail.com>
2020-04-24 00:07:47 -0300

    Inicio da Tela de Login do App

---
+++ AppProjeto/Projeto/app/src/main/java/com/example/projeto/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.projeto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+}
